e49472c9a663149330bda4cdd7b018fd67261e29
symphony <lichengyang@aliyn.com>
2018-05-20 16:51:43 +0800

    add docker-compose

--- Ubuntu/linux-init.sh
+++ Ubuntu/linux-init.sh
@@ -64,6 +64,14 @@ curl -L https://get.oh-my.fish | fish
 
 user$ curl -sSL https://get.daocloud.io/docker | sh
 
+##### Docker-compose
+
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
 ##### Install Oh-my-fish and A Theme
 
 omf install bobthefish
