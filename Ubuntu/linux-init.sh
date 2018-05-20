7682232e78b12e82a377badcaa05f113882e5539
Symphony <jd_test@jd-testdeMBP.lan>
2018-05-20 12:44:38 +0800

    add some things

--- Ubuntu/linux-init.sh
+++ Ubuntu/linux-init.sh
@@ -15,11 +15,11 @@ apt install sublime-text
 
 apt -y upgrade
 
-##### Add PPAs
+##### Add Common things
 
-apt install -y software-properties-commopn curl
+apt install -y software-properties-common curl
 
-##### Node.js
+##### Add Node.js
 
 curl --slient --location https://deb.nodesource.com/setup_8.x | bash -
 
@@ -31,13 +31,42 @@ apt update
 
 apt install -y build-essential dos2unix gcc git unzip make python2.7-dev python-pip re2c supervisor unattended-upgrades whois vim libnotify-bin pv cifs-utils bash-completion fish
 
+##### Install Node.js
 apt install -y nodejs
+apt install -y npm
 /usr/bin/npm install -g npm
+
+npm config set registry https://registry.npm.taobao.org
+
+npm i npm -g
+
 /usr/bin/npm install -g gulp-cli
 /usr/bin/npm install -g bower
 /usr/bin/npm install -g yarn
 /usr/bin/npm install -g grunt-cli
 
-npm config set registry https://registry.npm.taobao.org
 
 yarn config set registry https://registry.npm.taobao.org
+
+##### install oh my fish
+
+curl -L https://get.oh-my.fish | fish
+
+##### Docker
+
+user$ curl -sSL https://get.daocloud.io/docker | sh
+
+##### Install Oh-my-fish and A Theme
+
+omf install bobthefish
+
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Mono.ttf
+
+mv Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Mono.ttf /usr/share/fonts/truetype/
+
+fc-cache -f -v
+
+echo "set -g theme_nerd_fonts yes" >> ~/.config/fish/conf.d/omf.fish
+
+echo "##### Please Change Your Terminal Font to Sauce Code Pro Nerd Font #### \n"
+echo "##### Then the Theme will work #####"
