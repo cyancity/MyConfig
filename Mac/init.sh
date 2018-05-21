8a6e262232d917455a136af97a3d70ff4c114482
li-mac <lichengyang@aliyun.com>
2018-05-22 00:07:15 +0800

    add mac config

---
+++ Mac/init.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+####################  Install Homebrew #####################
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install brew ccat yarn node tmux go fish python3 wget 
+
+
+# Oh my fish
+
+
+# Install Spacemacs
+
+
+# Clone tmux conf
