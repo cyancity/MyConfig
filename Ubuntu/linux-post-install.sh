78ba4fa7710394aa24bcfdf3422282a55051d9bd
li-mac <lichengyang@aliyun.com>
2018-05-23 22:58:03 +0800

    add ss

--- Ubuntu/linux-post-install.sh
+++ Ubuntu/linux-post-install.sh
@@ -1,8 +1,19 @@
 
 #echo "set -g theme_nerd_fonts yes" >> ~/.config/fish/conf.d/omf.fish # if you have nerd font, uncomment this line
 
+# install shadowsocks-qt5
+add-apt-repository ppa:hzwhuang/ss-qt5
+apt-get update
+apt-get -y install shadowsocks-qt5
+
 chown -R $USER ~/.local/share/omf
 chown -R $USER ~/.cache/omf # oh my fish git repo dest
 
 omf install bobthefish # theme
 omf install proxy # proxy plugin
+
+#
+# touch ~/.config/omf/before.init.fish
+#echo "set proxy_host 127.0.0.1:1080
+#set proxy_auth false" >> ~/.config/omf/before.init.fish
+#
