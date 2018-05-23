f42d01fc6fe4c90906aee0af4a5a584ffa293ea9
li-mac <lichengyang@aliyun.com>
2018-05-23 22:45:36 +0800

    fix node.js install bug

--- Ubuntu/linux-init.sh
+++ Ubuntu/linux-init.sh
@@ -13,8 +13,9 @@ apt install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 apt update
-apt install sublime-text
 apt -y upgrade
+
+apt install sublime-text
 
 ##### Add Common things
 apt install -y software-properties-common curl
@@ -22,18 +23,15 @@ apt install -y software-properties-common curl
 ##### Add Node.js
 curl --slient --location https://deb.nodesource.com/setup_8.x | bash -
 
-apt-get install -y nodejs
+####################### Install Nodejs
 
 ##### Updates
 apt update
-
 ##### Basic Packages
 apt install -y build-essential dos2unix gcc git unzip make python2.7-dev python-pip re2c supervisor unattended-upgrades whois vim libnotify-bin pv cifs-utils bash-completion net-tools tmux
 
 ##### Install Node.js
 apt install -y nodejs
-apt install -y npm
-/usr/bin/npm install -g npm
 
 npm config set registry https://registry.npm.taobao.org
 
@@ -61,15 +59,13 @@ wget https://raw.githubusercontent.com/cyancity/MyConfig/master/Ubuntu/.tmux.con
 # older version of ubuntu should update fish shell
 apt-add-repository ppa:fish-shell/release-2
 apt-get update
-apt-get install fish
+apt-get -y install fish
 
 # chown dir priv
-chown -R $USER ~/.config/fish
+chown -R $USER ~/.config
 chown -R $USER ~/.local/share/fish
 chown -R $USER ~/.emacs.d
 
-# change dir priv
-chmod 777 -R /usr/local/lib/node_modules
 
 ##### Installing Oh-my-fish
 curl -L https://get.oh-my.fish | fish
--- Ubuntu/linux-post-install.sh
+++ Ubuntu/linux-post-install.sh
@@ -1,7 +1,6 @@
 
 #echo "set -g theme_nerd_fonts yes" >> ~/.config/fish/conf.d/omf.fish # if you have nerd font, uncomment this line
 
-chown -R $USER ~/.config/omf
 chown -R $USER ~/.local/share/omf
 chown -R $USER ~/.cache/omf # oh my fish git repo dest
 
